1dcf29aa027446c1c885b2bcf707f283d36a9ad7
Mwosa <nelsonmwosa7@gmail.com>
2022-10-06 09:38:28 +0300

    done several cards to view user details

--- userData.php
+++ userData.php
@@ -14,3 +14,113 @@
         $course = $row['course'];
     }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+		require_once('includes/links.php');
+	?>
+</head>
+<body>
+    <?php
+		include('includes/header.php');
+	?>
+    <div class="sidebar">
+		<?php
+			include('includes/sidebar.php');
+		?>	
+	</div>
+
+    <!-- card starts here -->
+    <div class="main-content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-6 py-4">
+                    <div class="card">
+                        <div class="card-header bg-dark text-white text-center">
+                            <span class="float-left"><i class="fa fa-user fa-lg"></i></span>
+                            <span>User Profile</span>
+                        </div>
+                        <div class="card-body">
+                            <ul class="list-group">
+                                <li class="list-group-item">Username:
+                                <span class="badge badge-primary badge-pill"><?php echo $name ?></span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-6 py-4">
+                    <div class="card">
+                        <div class="card-header bg-dark text-white text-center">
+                            <span class="float-left"><i class="fa fa-user fa-lg"></i></span>
+                            <span>User Profile</span>
+                        </div>
+                        <div class="card-body">
+                            <ul class="list-group">
+                                <li class="list-group-item">Reg Number:
+                                <span class="badge badge-primary badge-pill"><?php echo $reg_number ?></span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-6 py-4">
+                    <div class="card">
+                        <div class="card-header bg-dark text-white text-center">
+                            <span class="float-left"><i class="fa fa-user fa-lg"></i></span>
+                            <span>User Profile</span>
+                        </div>
+                        <div class="card-body">
+                            <ul class="list-group">
+                                <li class="list-group-item">Email:
+                                <span class="badge badge-primary badge-pill"><?php echo $email ?></span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-6 py-4">
+                    <div class="card">
+                        <div class="card-header bg-dark text-white text-center">
+                            <span class="float-left"><i class="fa fa-user fa-lg"></i></span>
+                            <span>User Profile</span>
+                        </div>
+                        <div class="card-body">
+                            <ul class="list-group">
+                                <li class="list-group-item">Phone No.:
+                                <span class="badge badge-primary badge-pill"><?php echo $phone ?></span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-header bg-dark text-white text-center">
+                            <span class="float-left"><i class="fa fa-user fa-lg"></i></span>
+                            <span>User Profile</span>
+                        </div>
+                        <div class="card-body">
+                            <ul class="list-group">
+                                <li class="list-group-item">Course:
+                                <span class="badge badge-primary badge-pill"><?php echo $course ?></span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+  
+<script src="jquery.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>    
+</body>
+</html>
